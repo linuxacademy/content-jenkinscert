fe768f81ac7914665cbc5034061cd4ca99900505
darealmc <iamdarealmc@gmail.com>
2018-10-30 15:31:33 -0500

    added unit tests

---
+++ src/test/java/Test2.java
@@ -0,0 +1,13 @@
+package launch;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+
+public class Test2 {
+          @Test
+	  public void run() {
+		 Assert.assertEquals(2 + 2, 4); 
+           }
+}
+
