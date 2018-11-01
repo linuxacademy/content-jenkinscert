8a9457575d879594625933180ea198e3ab5093ca
darealmc <michael.mcclaren@linuxacademy.com>
2018-11-01 07:34:26 -0500

    adding back in testing

---
+++ src/test/java/test1.java
@@ -0,0 +1,19 @@
+//package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class hellotest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+
+
+}
